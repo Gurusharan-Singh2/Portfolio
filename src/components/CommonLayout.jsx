2c046feb64fa685b3a97cedbf5eb786a8ebef78f
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-04-22 07:00:44 +0530

    22-04 (3) commit

--- src/components/CommonLayout.jsx
+++ src/components/CommonLayout.jsx
@@ -22,7 +22,7 @@ const CommonLayout = ({ children }) => {
 
     
     <div key={pathname} className="w-screen min-h-screen h-auto bg-gradient-to-b from-blue-200 to-violet-200 dark:bg-none dark:bg-zinc-900">
-      <motion.div className='h-screen w-screen fixed bg-black rounded-b-[100px] z-40'
+      <motion.div className='h-screen w-screen fixed bg-black dark:bg-white rounded-b-[100px] z-40'
       animate={{height:"0vh"}} 
       exit={{
         height:"140vh"
@@ -31,7 +31,7 @@ const CommonLayout = ({ children }) => {
         duration:0.5,
         ease:"easeOut"
       }}/>
-      <motion.div className='fixed m-auto w-fit h-fit top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50'
+      <motion.div className='fixed m-auto w-fit h-fit top-0 bottom-0 left-0 right-0 text-white dark:text-black text-3xl sm:text-4xl md:text-6xl lg:text-8xl cursor-default z-50'
       initial={{opacity:1}}
       animate={{opacity:0}}
       transition={{duration:0.8, ease:"easeOut"}}>
@@ -39,7 +39,7 @@ const CommonLayout = ({ children }) => {
     {pathname.substring(1).toUpperCase()}
 
       </motion.div>
-      <motion.div className='h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30'
+      <motion.div className='h-screen w-screen fixed bg-black dark:bg-white rounded-t-[100px] bottom-0 z-30'
       initial={{height:"140vh"}}
       animate={{
         height:"0vh",
